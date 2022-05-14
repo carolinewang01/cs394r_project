1f86b497a985f03118f5869a6eefe8ee605dc42c
cuijiaxun <cuijiaxun_sjtu@163.com>
2022-05-14 09:32:15 -0500

    specify the pettingzoo version in the install script

--- setup/install.sh
+++ setup/install.sh
@@ -6,11 +6,11 @@ conda create -n ${CONDA_NAME} python=3.7 -y;
 conda activate ${CONDA_NAME};
 
 conda install cudatoolkit=10.1;
-pip install torch===1.4.0 -f https://download.pytorch.org/whl/torch_stable.html;
+pip install torch==1.4.0 -f https://download.pytorch.org/whl/torch_stable.html;
 pip install tianshou==0.4.7;
 pip install --upgrade open_spiel;
 pip install gym==0.19.0;
-pip install opencv-python matplotlib tqdm pettingzoo;
+pip install opencv-python matplotlib tqdm pettingzoo==1.17.0;
 
 # extra commands to run tianshou's IQN example with atari
 pip install gym\[atari\];
