50cfbc62ba249eaaa3ff2f62a1f608e05290a663
Jiaxun Cui <cuijiaxun_sjtu@163.com>
2022-05-17 19:48:54 -0500

    setup

--- setup/install.sh
+++ setup/install.sh
@@ -6,7 +6,7 @@ conda create -n ${CONDA_NAME} python=3.7 -y;
 conda activate ${CONDA_NAME};
 
 conda install cudatoolkit=10.1;
-pip install torch==1.4.0 -f https://download.pytorch.org/whl/torch_stable.html;
+conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch;
 pip install tianshou==0.4.7;
 pip install --upgrade open_spiel;
 pip install gym==0.19.0;
